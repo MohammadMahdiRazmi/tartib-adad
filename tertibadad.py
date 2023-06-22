66903c9ccaf525c2039d103ad54cddac98c0f381
MahdiRazmi <109846619+MohammadMahdiRazmi@users.noreply.github.com>
2023-06-22 10:55:27 +0330

    Update tertibadad.py

--- tertibadad.py
+++ tertibadad.py
@@ -5,24 +5,42 @@ if (num1<num2<num3):
     big=num3
     meduim=num2
     small=num1
-if (num1<num3<num2):
+elif (num1<num3<num2):
     big=num2
     meduim=num3
     small=num1
-if (num2<num1<num3):
+elif (num2<num1<num3):
     big=num3
     meduim=num1
     small=num2
-if (num2<num3<num1):
+elif (num2<num3<num1):
     big=num1
     meduim=num3
     small=num2 
-if (num3<num2<num1):
+elif (num3<num2<num1):
     big=num1
     meduim=num2
     small=num3
-if (num3<num1<num2):
+elif (num3<num1<num2):
     big=num2
     meduim=num1
     small=num3
+elif(num1==num2<num3):
+    big=num3
+    meduim=num2
+    small=num1
+elif(num2==num3<num1):
+    big=num1
+    meduim=num2
+    small=num3
+elif(num1==num3<num2):
+    big=num2
+    meduim=num3
+    small=num1
+elif(num1==num3>num2):
+    big=num1
+    meduim=num3
+    small=num2
+else:
+    print("error not valid")
 print(big,meduim,small)
